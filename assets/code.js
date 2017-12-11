2c8f4efb954789c2d4937d44b8d742521b1fd11a
ymatuhin <ym@ymatuhin.ru>
2017-12-11 14:44:51 +0300

    Add another polyfill

--- assets/code.js
+++ assets/code.js
@@ -1,4 +1,6 @@
 (function() {
+    Function.prototype.bind=Function.prototype.bind||function(b){if(typeof this!=="function"){throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");}var a=Array.prototype.slice,f=a.call(arguments,1),e=this,c=function(){},d=function(){return e.apply(this instanceof c?this:b||window,f.concat(a.call(arguments)));};c.prototype=this.prototype;d.prototype=new c();return d;};
+
     const DEFAULT_SETTINGS = ["last 4 version"];
     var timer = null;
 
