729ac2550da28bd7887a8306debb9a7d3e6b8ca3
ymatuhin <ym@ymatuhin.ru>
2017-12-23 00:34:20 +0300

    Rewrite JS code, add parceljs with babel

---
+++ src/main.js
@@ -0,0 +1,100 @@
+import store from 'store';
+import langRedirect from './redirect.js';
+import autoprefixer from './autoprefixer.min.js';
+import { highlightElement } from './highlight.js';
+
+const DEFAULT_BROWSERS = ["last 4 version"];
+
+class App {
+    constructor() {
+        this.vars();
+        this.init();
+    }
+
+    vars() {
+        this.browserList = store.get('autoprefixer:browsers') || DEFAULT_BROWSERS;
+
+        this.$leftPane = document.querySelector(".js-input");
+        this.$rightPane = document.querySelector(".js-output");
+        this.$filterForm = document.querySelector(".js-filter");
+        this.$textFilter = document.querySelector(".js-browsers-filter")
+        this.$browserListLink = document.querySelector(".js-link-browserlist")
+        this.$selectButton = document.querySelector(".js-select");
+    }
+
+    init() {
+        this.$textFilter.value = this.browserList.join(', ');
+        this.$leftPane.focus();
+
+        store.remove('browsers');
+        this.listeners();
+        this.updateBrowserListLink();
+        this.runPrefixer();
+    }
+
+    listeners() {
+        this.$leftPane.addEventListener("keyup", this.runPrefixer.bind(this), false);
+        this.$filterForm.addEventListener("submit", this.filterUpdate.bind(this), false);
+
+        this.$selectButton.addEventListener("click", this.selectResult.bind(this), false);
+        this.$textFilter.addEventListener("keyup", this.updateBrowserListLink.bind(this), false);
+        this.$leftPane.addEventListener("scroll", this.scrollSync.bind(this), false);
+        this.$rightPane.addEventListener("scroll", this.scrollSync.bind(this), false);
+    }
+
+    runPrefixer() {
+        const inputCSS = this.$leftPane.value;
+        const params = { browsers: this.browserList };
+
+        try {
+            const compiled = autoprefixer.process(inputCSS, {}, params);
+            this.$rightPane.innerHTML = this.textPrepare(compiled.css);
+            highlightElement(this.$rightPane)
+        } catch (error) {
+            this.$rightPane.innerHTML = this.textPrepare(error.toString());
+        }
+    }
+
+    textPrepare(text = '') {
+        return text.replace(/\</g, "&lt;").replace(/\>/g, "&gt;");
+    }
+
+    filterUpdate(event) {
+        event.preventDefault();
+
+        const browsers = this.$textFilter.value.split(",");
+        const trimmedBrowsers = browsers.map(browser => browser.trim());
+        const newValue = trimmedBrowsers.length ? trimmedBrowsers : DEFAULT_SETTINGS;
+
+        this.browserList = newValue;
+        store.set('autoprefixer:browsers', newValue)
+        this.runPrefixer();
+    }
+
+    selectResult() {
+        if (document.createRange) {
+            var rng, sel;
+            rng = document.createRange();
+            if (rng.selectNode) rng.selectNode(this.$rightPane);
+            sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(rng);
+        } else if (document.body && document.body.createTextRange) {
+            var rng;
+            rng = document.body.createTextRange();
+            rng.moveToElementText(this.$rightPane);
+            rng.select();
+        }
+    }
+
+    updateBrowserListLink() {
+        this.$browserListLink.href = encodeURI('http://browserl.ist/?q=' + this.$textFilter.value);
+    }
+
+    scrollSync(event) {
+        if (event.target === this.$leftPane) this.$rightPane.scrollTop = this.elems.left.scrollTop;
+        else this.elems.left.scrollTop = this.$rightPane.scrollTop;
+    }
+}
+
+if (!langRedirect()) new App();
---
+++ src/redirect.js
@@ -0,0 +1,17 @@
+const HOME_PAGE_LANG = 'en';
+const PATHES = { ru: '/ru/', en: '/' };
+
+const userBrowserLang = navigator.language || navigator.userLanguage;
+const userLang = userBrowserLang.split('-')[0].toLowerCase() || HOME_PAGE_LANG;
+const path = location.pathname.replace(/\//g, '') || HOME_PAGE_LANG;
+
+export default () => {
+  if (userLang === path) return false;
+  if (!sessionStorage) return false;
+  if (sessionStorage.redirected) return false;
+  if (!PATHES[userLang]) return;
+
+  sessionStorage.redirected = true;
+  location.pathname = PATHES[userLang];
+  return true;
+};
